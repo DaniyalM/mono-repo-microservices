9d97cf53a653e327dc1263d1d8081b825c75833a
DanLunara <daniyal@nodpay.app>
2024-07-30 00:52:14 -0700

    Gateway initialized

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { GatewayModule } from './gateway.module';
+import * as cookieParser from "cookie-parser";
+import { ValidationPipe } from '@nestjs/common';
+import { Logger } from "nestjs-pino";
+import { ConfigService } from '@nestjs/config';
+
+
 
 async function bootstrap() {
   const app = await NestFactory.create(GatewayModule);
-  await app.listen(3000);
+  app.use(cookieParser());
+  app.useGlobalPipes(new ValidationPipe());
+  app.useLogger(app.get(Logger))
+  const configService = app.get(ConfigService)
+  await app.listen(configService.get<number>('HTTP_PORT'));
 }
 bootstrap();
